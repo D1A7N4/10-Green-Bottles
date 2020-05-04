c7a3938852f76c5e923d083b8816dcf2b2d8b859
Daniel Seavers <daniel.p.seavers@gmail.com>
2020-05-04 12:22:46 +0100

    Updated to allow user input of number of bottles

--- 10 Green Bottles/Program.cs
+++ 10 Green Bottles/Program.cs
@@ -3,18 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace _10_Green_Bottles
 {
     class Program
     {
         static void Main(string[] args)
         {
-            for (int i = 10; i >1; i--)
+            Console.WriteLine("Please enter the number of green bottles standing on your wall.");
+            int j;
+            while (int.TryParse(Console.ReadLine(), out j) == false)
             {
-                Console.WriteLine("There were " + i + " green bottles standing on the wall" +Environment.NewLine+ i + " green bottles standing on the wall" + Environment.NewLine + "And if one green bottle should accidently fall" + Environment.NewLine + "There'd be " + (i - 1) + " green bottles standing on the wall" + Environment.NewLine);
+                Console.WriteLine("Please enter a valid integer number of bottles to be stood on your wall.");
+            }
+            for (int i = j; i > 1; i--)
+            {
+                Console.WriteLine("There were " + i + " green bottles standing on the wall" + Environment.NewLine + i + " green bottles standing on the wall" + Environment.NewLine + "And if one green bottle should accidently fall" + Environment.NewLine + "There'd be " + (i - 1) + " green bottles standing on the wall" + Environment.NewLine);
             }
             Console.WriteLine("There was 1 green bottle standing on the wall" + Environment.NewLine + "1 green bottle standing on the wall" + Environment.NewLine + "And if one green bottle should accidently fall" + Environment.NewLine + "There'd be no green bottles standing on the wall" + Environment.NewLine);
+            Console.ReadLine();
         }
     }
 }
